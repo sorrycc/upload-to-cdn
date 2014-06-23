c1dd1398087558817b5e1dcfdf5cdfc45dac8eb3
chencheng <sorrycc@gmail.com>
2014-06-23 18:46:11 +0800

    add cache for upload

--- index.js
+++ index.js
@@ -1,20 +1,88 @@
+var mkdirp = require('mkdirp');
+var join = require('path').join;
+var crypto = require('crypto');
+var util = require('util');
+var debug = require('debug')('utc');
+var fs = require('fs');
+var exists = fs.existsSync;
+var read = fs.readFileSync;
 
 var engines = {
   tps: require('tps'),
   cdn: require('cdn')
 };
 
+var cachePath = join(process.env.HOME, '.cdncache');
+mkdirp.sync(cachePath);
+
 module.exports = function(files, opts) {
   if (typeof files === 'string') {
     files = [files];
   }
-  opts.engineType = opts.engineType || 'tps';
-  upload(files, engineType, cb || noop);
+
+  var result = {};
+
+  // Flow:
+  // 1. read cache
+  // 2. filter and upload uncached files
+  // 3. concat cache and uncache results
+  // 4. write cache
+  // 5. callback
+
+  // 1
+  var md5hashs = md5(files);
+  debug('1: %s', md5hashs);
+  // 2
+  var uncacheFiles = files.filter(function(f, i) {
+    var md5hash = md5hashs[i];
+    var cacheFile = join(cachePath, md5hash);
+    if (exists(cacheFile)) {
+      var url = read(cacheFile, 'utf-8');
+      if (url) result[f] = url;
+      return false;
+    } else {
+      return true;
+    }
+  });
+  debug('2: uncacheFiles: %s', uncacheFiles);
+
+  var engine = engines[opts.type || 'tps'];
+  engine(files, function(e, urls) {
+    if (e) throw Error(e);
+    debug('2: urls: %s', urls);
+    // 3
+    uncacheFiles.forEach(function(f, i) {
+      result[f] = urls[i];
+    });
+    debug('3: result: %s', result);
+
+    // 4
+    writeCache(result);
+
+    // 5
+    opts.callback && opts.callback(null, files.map(function(f) {
+      return result[f];
+    }));
+  });
 };
 
-function upload(files, engineType, cb) {
-  var engine = engines[engineType];
-  engine(files, cb);
+function md5(files) {
+  if (!util.isArray(files)) {
+    files = [files];
+  }
+  return files.map(function md5Handler(str) {
+    var md5sum = crypto.createHash('md5');
+    return md5sum.update(str).digest('hex');
+  });
 }
 
-function noop() {}
+function writeCache(o) {
+  var files = Object.keys(o);
+  var md5hashs = util.md5(files);
+  files.map(function(f, i) {
+    var md5hash = md5hashs[i];
+    var url = o[f];
+    var cacheFile = join(cachePath, md5hash);
+    fs.writeFileSync(cacheFile, url);
+  });
+}
